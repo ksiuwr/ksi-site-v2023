4baaba0b6ea20a1d2bc3afb9869624b1012d9ccf
szymon-f <sfluder25@gmail.com>
2023-01-25 17:27:49 +0100

    put contact page inside of Layout component

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "../../styles/global.css"
 import * as styles from "../../styles/contact.module.css"
-
+import Layout from '../../components/Layout'
 
 export default function Index(){
 
@@ -15,24 +15,25 @@ export default function Index(){
     ]
 
     return (
-        <div className={styles.page}>
-            <div className={`${styles.container}`}>
-                <h1 className={`mobile_header ${styles.header_section}`}>Skontaktuj się</h1>
-                {/*<div className={styles.line_subject}></div>*/}
-                <ul>
-                    {options.map(opt => (
-                        <li className={styles.listed_elem} key={opt.id}>
-                            <img width={"48px"} src={opt.src} alt={"sub_icon"}/>
-                            <p className={"body_1"}>{opt.name}</p>
-                        </li>
-                    ))}
-                </ul>
-                <div className={styles.logo}>
-                    <img src={"/assets/img.png"}  alt={"logo"}/>
+        <Layout>
+            <div className={styles.page}>
+                <div className={`${styles.container}`}>
+                    <h1 className={`mobile_header ${styles.header_section}`}>Skontaktuj się</h1>
+                    {/*<div className={styles.line_subject}></div>*/}
+                    <ul>
+                        {options.map(opt => (
+                            <li className={styles.listed_elem} key={opt.id}>
+                                <img width={"48px"} src={opt.src} alt={"sub_icon"}/>
+                                <p className={"body_1"}>{opt.name}</p>
+                            </li>
+                        ))}
+                    </ul>
+                    <div className={styles.logo}>
+                        <img src={"/assets/img.png"}  alt={"logo"}/>
+                    </div>
+                    <div className={styles.header_section}></div>
                 </div>
-                <div className={styles.header_section}></div>
             </div>
-        </div>
-
+        </Layout>
     )
 }
