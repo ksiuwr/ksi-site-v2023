eac695a6ec29dc59a9bd7a6bf18d188cef0ff666
szymon-f <sfluder25@gmail.com>
2023-01-25 17:16:23 +0100

    do Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Link } from 'gatsby'
+import "../styles/global.css"
+import * as styles from "../styles/footer.component.css"
+
+export default function Footer(){
+    return(
+        <div className={styles.footer_container}>
+          <ul className={styles.footer_info_list}>
+            <li className={styles.footer_info_element}>
+              <p>Instytut Informatyki Uniwersytetu Wrocławskiego ul. Fryderyka Joliot-Curie 15,  Wrocław, Sala 24</p>
+            </li>
+            <li className={styles.footer_info_element}>
+              <p>ksi@cs.uni.wroc.pl</p>
+            </li>
+            <li className={styles.footer_info_element}>
+              <p>KSI</p>
+            </li>
+          </ul>
+        </div>
+    )
+}
